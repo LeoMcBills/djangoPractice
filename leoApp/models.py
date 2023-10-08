661b333acc871cef56af794d8609295de4c6cfc7
LeoMcBills <leokinyera9@gmail.com>
2023-10-08 14:44:00 +0300

    made new migrations

--- leoApp/models.py
+++ leoApp/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Feature(models.Model):
-    name: models.CharField(max_length=100)
-    detail: models.CharField(max_length=1000)
+    name =  models.CharField(max_length=100, default="")
+    detail = models.CharField(max_length=500, default="")
