c1bf5f77e7da5def2741eaf63902630b8748cb59
LeoMcBills <leokinyera9@gmail.com>
2023-10-08 13:34:54 +0300

    add Smart vtracker

--- leoApp/views.py
+++ leoApp/views.py
@@ -4,15 +4,16 @@ from .models import Feature
 
 # Create your views here.
 def index(request):
-    feature1 = Feature()
-    feature1.id = 1
-    feature1.name = 'app1'
-    feature1.detail = 'This is app1'
 
     feature2 = Feature()
     feature2.id = 2
     feature2.name = 'Smart vTracker'
     feature2.detail = 'We offer advanced already-made and customized tracking solutions to individuals, companies and governments.'
+
+    feature1 = Feature()
+    feature1.id = 1
+    feature1.name = 'app1'
+    feature1.detail = 'This is app1'
 
     feature = [feature1, feature2,]
     return render(request, 'index.html', {'feature' : feature})
