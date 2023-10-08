aacdd00ce8a977250669e79f8d1b3592dace66b7
LeoMcBills <leokinyera9@gmail.com>
2023-10-08 13:16:35 +0300

    use models

--- leoApp/views.py
+++ leoApp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Feature
 # from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    Feature1 = Feature()
+    Feature1.id = 1
+    Feature1.name = 'app1'
+    Feature1.detail = 'This is app1'
+    return render(request, 'index.html', {'feature' : Feature1})
